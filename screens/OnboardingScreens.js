4de1325901f9e2d2e16cdd012d373b551903df66
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-06-27 12:47:40 +0300

    react-native-onboarding-swiper

--- screens/OnboardingScreens.js
+++ screens/OnboardingScreens.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import {  Text, View, Button, StyleSheet } from 'react-native'
+import {  Text, View, Button, StyleSheet, Image } from 'react-native'
 import Onboarding from 'react-native-onboarding-swiper';
 
 
 const OnboardingScreens = ( { navigation } ) => {
   return (
     <Onboarding
+      onSkip = { () => navigation.navigate('Login') }
+      onDone = { () => navigation.navigate('Login') }
       pages={[
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('./images/circle.png')} />,
-          title: 'Onboarding 1',
-          subtitle: 'Done with React Native Onboarding swiper',
+          backgroundColor: '#a6e4d0',
+          image: <Image source={require('../assets/pic7.jpg')} />,
+          title: 'Connect to the world',
+          subtitle: 'New way to connect to the world',
         },
         {
-          backgroundColor: '#fff',
-          image: <Image source={require('./images/circle.png')} />,
-          title: 'Onboarding 1',
-          subtitle: 'Done with React Native Onboarding swiper',
+          backgroundColor: '#fdeb93',
+          image: <Image source={require('../assets/pic4.jpg')} />,
+          title: 'Share your favorites',
+          subtitle: 'Share oyur things with other friends',
+        },
+        {
+          backgroundColor: '#ffc0cb',
+          image: <Image source={require('../assets/pic8.jpg')} />,
+          title: 'Become the star',
+          subtitle: 'Let the inspire take you to the sky',
         }
       ]}
     />
